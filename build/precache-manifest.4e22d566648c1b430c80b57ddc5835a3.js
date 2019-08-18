a0c4c53440b386c5cf97f75351adf04158fc0575
David Piper <black.god1930@gmail.com>
2019-08-18 07:21:06 -0400

    transfered all older data from redux--01 to redux--02

---
+++ build/precache-manifest.4e22d566648c1b430c80b57ddc5835a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f719b3d1696d2c0f4e91e12aaf442327",
+    "url": "/redux--01-start/index.html"
+  },
+  {
+    "revision": "d6902a4cf55ea5d2aa89",
+    "url": "/redux--01-start/static/css/main.6e44ffb7.chunk.css"
+  },
+  {
+    "revision": "063139daf9895fa98bcc",
+    "url": "/redux--01-start/static/js/2.243da359.chunk.js"
+  },
+  {
+    "revision": "d6902a4cf55ea5d2aa89",
+    "url": "/redux--01-start/static/js/main.548ab929.chunk.js"
+  },
+  {
+    "revision": "5da0fbc0dba409af347d",
+    "url": "/redux--01-start/static/js/runtime~main.ab42f7d3.js"
+  }
+]);
